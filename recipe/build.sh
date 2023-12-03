5085944194d2eaf47480515886e044453b283150
looooo <sppedflyer@gmail.com>
2023-12-03 13:02:01 +0100

    add rpath-patch

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,8 +1,3 @@
-if [[ ${HOST} =~ .*darwin.* ]]; then
-    rm -rf /usr/local/opt/libomp || echo "trying to remove /usr/local/opt/libomp failed"
-    rm -rf /opt/homebrew/opt/libomp || echo "trying to remove /opt/homebrew/opt/libomp failed"
-fi
-
 export CMAKE_ARGS="${CMAKE_ARGS} -D BUILD_DOC:BOOL=OFF -D VERSION_STRING:STRING=${PKG_VERSION}"
 
 ${PYTHON} -m pip install . -vv
